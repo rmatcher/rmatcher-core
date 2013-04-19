cd66747f2112decfad7791e00237f9a03f91da29
Ameen Askar <ameenaskar@ucla.edu>
2013-04-18 20:26:15 -0700

    added main method under Matcher

---
+++ src/main/java/com/rmatcher/core/matcher/Matcher.java
@@ -0,0 +1,15 @@
+package com.rmatcher.core.matcher;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ameen
+ * Date: 4/18/13
+ * Time: 8:19 PM
+ */
+import java.io.*;
+
+public class Matcher {
+    public static void main(String [] args) throws IOException {
+
+    }
+}
